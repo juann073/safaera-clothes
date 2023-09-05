e23c32c9cbe58f5dbdd377e9040a511767e2438a
juan losada <juanlosada073@gmail.com>
2023-09-04 21:27:30 -0300

    solucionando errores

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,21 +1,20 @@
 import { NavLink } from "react-router-dom"
-
+import ItemCount from "../ItemCount/ItemCount"
 
 
 const Item = ({info}) =>{
-    return(
-        
-            <div className="Cards">
-                <img className="img-card" src={info.Image} alt="" />
-                <p className="tittle">{info.name}</p>
-                <p className="price">${info.price}</p>
-                <NavLink to="/item/:id"><button>Detalle</button></NavLink>
-            </div>
-        
-        
+return(
 
-        )
+<div className="Cards">
+<img className="img-card" src={info.Image} alt={info.detail} />
+<p className="tittle">{info.name}</p>
+<p className="price">${info.price}</p>
+<NavLink to={`/item/${info.id}`}><button>Ver detalle</button> </NavLink>
+
+</div>
+
+
+)
 }
 
 export default Item
-
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -8,10 +8,11 @@ const ItemDetail = ({data}) =>{
     return(
         <div className="container">
             <div className="detail">
-                <img className="detail-image" src="" alt="" />
+                <img className="img-card" src={data.Image} alt={data.detail} />
                 <div className="content">
                     <h1>{data.name}</h1>
-
+                    <p>${data.price}</p>
+                    <p>{data.detail}</p>
                     <ItemCount inintial={1} stock={5}/>
                 </div>
             </div>
